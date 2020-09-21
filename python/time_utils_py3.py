4feeae83361a15262a881e887707dabdfcd83320
farhan85 <4463436+farhan85@users.noreply.github.com>
2020-09-20 21:57:35 -0700

    Add example of timezone aware datetime in python3

---
+++ python/time_utils_py3.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python3
+
+import pytz
+
+from calendar import timegm
+from datetime import datetime, timezone
+
+
+tz_pdt = pytz.timezone("America/Los_Angeles")
+
+
+def now_utc():
+    return datetime.now(timezone.utc)
+
+def to_epoch(dt):
+    return timegm(dt.astimezone(timezone.utc).timetuple())
+
+def now_epoch():
+    return timegm(datetime.now(timezone.utc).timetuple())
+
+def to_datetime(epoch=None, str_dt=None, tz=timezone.utc):
+    if str_dt:
+        return datetime.strptime(str_dt, '%Y-%m-%d %H:%M:%S').astimezone(tz)
+    elif epoch:
+        return datetime.fromtimestamp(epoch, tz=tz)
+
+
+now = now_utc()
+print("now (local):", now.astimezone(tz_pdt))
+print("now (utc):", now.astimezone(timezone.utc))
+print("now (epoch):", to_epoch(now))
+print()
+
+
+print('Buiding datetime from constructor')
+print('Starting date: 2012-04-01 00:00 PDT')
+local_dt = tz_pdt.localize(datetime(2012, 4, 1, 0, 0))
+# prints 1333238400
+epoch = to_epoch(local_dt)
+print('Epoch:', epoch)
+print('Convert to local:', to_datetime(epoch=epoch).astimezone(tz_pdt))
+print('Convert to UTC:', to_datetime(epoch=epoch).astimezone(timezone.utc))
+print()
+
+
+print('Buiding datetime from UTC string')
+utc_dt = to_datetime(str_dt='2018-06-07 05:10:05')
+print('UTC dt:', utc_dt)
+epoch = to_epoch(utc_dt)
+print('Epoch:', epoch)
+print('Convert to local:', to_datetime(epoch=epoch).astimezone(tz_pdt))
+print('Convert to UTC:', to_datetime(epoch=epoch).astimezone(timezone.utc))
+print()
+
+
+print('Buiding datetime from PDT string')
+local_dt = to_datetime(str_dt='2018-06-07 13:00:00', tz=tz_pdt)
+print('Local dt:', local_dt)
+epoch = to_epoch(local_dt)
+print('Epoch:', epoch)
+print('Convert to local:', to_datetime(epoch=epoch).astimezone(tz_pdt))
+print('Convert to UTC:', to_datetime(epoch=epoch).astimezone(timezone.utc))
