1a5416463c980802a00f0dc82a8aa4ea55eff13a
farhan85 <4463436+farhan85@users.noreply.github.com>
2022-08-11 20:57:18 +0000

    Add json2csv python script

---
+++ python/json2csv.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python
+"""
+Converts json files of the form list-of-objects into CSV files, and vice versa.
+
+When converting JSON-to-CSV, the CSV header comes from the keys of the first
+JSON object. All other JSON objects must have the same keys as the first one.
+
+Example (JSON-to-CSV):
+
+  > cat input.json
+  [
+    { "field1": 1, "field2": 2, "field3": 3 },
+    { "field1": 4, "field2": 5, "field3": 6 },
+    { "field1": 7, "field2": 8, "field3": 9 }
+  ]
+
+  > python json2csv.py -f input.json
+  field1,field2,field3
+  1,2,3
+  4,5,6
+  7,8,9
+
+Example (JSON-to-CSV):
+
+  > cat input.csv
+   field1,field2,field3
+   1,2,3
+   4,5,6
+   7,8,9
+
+   > python json2csv.py -f input.csv -j
+   [{ "field1": 1, "field2": 2, "field3": 3 },...
+"""
+
+import argparse
+import contextlib
+import csv
+import json
+import sys
+
+
+@contextlib.contextmanager
+def file_reader(filename=None, **kwargs):
+    reader = open(filename, 'r', **kwargs) if filename else sys.stdin
+    yield reader
+    if filename: reader.close()
+
+
+def to_csv(input_filename):
+    with file_reader(input_filename) as f:
+        data = json.load(f)
+    fields = data[0].keys()
+
+    csv_writer = csv.DictWriter(sys.stdout, fieldnames=fields)
+    csv_writer.writeheader()
+    for datum in data:
+        csv_writer.writerow(datum)
+
+
+def to_json(input_filename):
+    with file_reader(input_filename, newline='') as f:
+        csv_reader = csv.DictReader(f)
+    data = list(csv_reader)
+    print(json.dumps(data))
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description=__doc__,
+        formatter_class=argparse.RawTextHelpFormatter)
+    parser.add_argument('-f', '--file', help='Input file (read from stdin if not provided)')
+    parser.add_argument('-r', '-j', '--json', help='Reverse operation (Convert to JSON)', action='store_true')
+    args = parser.parse_args()
+
+    if args.json:
+        to_json(args.file)
+    else:
+        to_csv(args.file)
