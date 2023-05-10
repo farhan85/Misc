f17f91ee9ce6fe935854a1a6cc20a78085d1c6e6
farhan85 <4463436+farhan85@users.noreply.github.com>
2023-05-10 17:41:07 +0000

    Update percentiles to be python3 compatible
    Also remove the if-not-none check to use the "easier to ask for
    forgiveness" coding convention

--- python/percentile.py
+++ python/percentile.py
@@ -11,9 +11,6 @@ def percentile(samples, percent):
     :rtype: the percentile of the values
     """
 
-    if not samples:
-        return None
-
     samples = sorted(samples)
 
     rank = percent * (len(samples) - 1)
@@ -31,11 +28,10 @@ def percentile(samples, percent):
     return lower_val + frac*(upper_val - lower_val)
 
 
-a = range(20,3, -1)
 a = [79, 67, 44, 68, 30, 13, 22, 49, 65, 20, 20, 46, 65, 69, 58, 28, 27, 47, 14, 78, 60, 77, 79]
-print percentile(a, 0.0)
-print percentile(a, 0.25)
-print percentile(a, 0.5)
-print percentile(a, 0.95)
-print percentile(a, 0.99)
-print percentile(a, 1.0)
+print(percentile(a, 0.0))
+print(percentile(a, 0.25))
+print(percentile(a, 0.5))
+print(percentile(a, 0.95))
+print(percentile(a, 0.99))
+print(percentile(a, 1.0))
