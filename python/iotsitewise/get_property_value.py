4212f605e94952a3c7024b45267caba98f5047c0
farhan85 <4463436+farhan85@users.noreply.github.com>
2024-11-23 11:24:58 -0800

    Better formatting of historical property values

--- python/iotsitewise/get_property_value.py
+++ python/iotsitewise/get_property_value.py
@@ -2,11 +2,16 @@
 
 import json
 import os
+import pytz
 import re
+from tabulate import tabulate
 from datetime import datetime, timedelta, timezone
 
 import boto3
 import click
+
+
+TZ_PST = pytz.timezone("America/Los_Angeles")
 
 
 def now_utc():
@@ -30,6 +35,30 @@ def create_entry(entry_id, asset_id, property_id, alias, quality, start_dt, end_
         entry['qualities'] = [quality.upper()]
         entry['timeOrdering'] = 'ASCENDING'
     return entry
+
+
+def extract_property_data(property_value):
+    dt = datetime.fromtimestamp(property_value['timestamp']['timeInSeconds'], tz=TZ_PST)
+    dt_nanos = property_value['timestamp']['offsetInNanos']
+    value = list(property_value['value'].values())[0]
+    quality = property_value['quality']
+    return (f'{dt} {dt_nanos}ns', value, quality)
+
+
+def print_success_entries(entries):
+    if len(entries) > 1:
+        raise RuntimeError('Expected only one entry', entries)
+
+    entry = entries[0]
+    if 'assetPropertyValueHistory' in entry:
+        headers = ['Timestamp', 'Value', 'quality']
+        row_data = [extract_property_data(v) for v in entry['assetPropertyValueHistory']]
+        print(tabulate(row_data, headers=headers, tablefmt="presto"))
+    else:
+        dt, value, quality = extract_property_data(entry['assetPropertyValue'])
+        print('Timestamp:', dt)
+        print('Value:    ', value)
+        print('Quality:  ', quality)
 
 
 @click.command(context_settings={'help_option_names': ['-h', '--help']})
@@ -58,13 +87,14 @@ def main(asset_id, property_id, alias, minutes, quality, dry_run):
         response = iotsitewise_client.batch_get_asset_property_value_history(entries=entries)
     else:
         response = iotsitewise_client.batch_get_asset_property_value(entries=entries)
+    print("RequestId:", response['ResponseMetadata']['RequestId'])
 
-    print("RequestId: ", response['ResponseMetadata']['RequestId'])
     error_messages = [entry['errorMessage'] for entry in response.get('errorEntries', [])]
     if error_messages:
         print('Errors:', '\n'.join(error_messages))
-    else:
-        print(json.dumps(response['successEntries']))
+        return
+
+    print_success_entries(response['successEntries'])
 
 
 if __name__ == '__main__':
