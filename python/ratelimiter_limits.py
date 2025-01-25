9e36055aaca1b5634b6b3e36df2bd38a4ad9ae34
farhan85 <4463436+farhan85@users.noreply.github.com>
2025-01-25 00:56:58 -0800

    Create wrapper func for ratelimiter

--- python/ratelimiter_limits.py
+++ python/ratelimiter_limits.py
@@ -1,4 +1,5 @@
 import time
+from functools import lru_cache
 from datetime import datetime, timezone
 from limits import RateLimitItemPerSecond, storage, strategies
 
@@ -6,6 +7,29 @@ from limits import RateLimitItemPerSecond, storage, strategies
 ratelimiter = strategies.MovingWindowRateLimiter(storage.MemoryStorage())
 limit = RateLimitItemPerSecond(2)
 
+
+@lru_cache
+def get_limit(tps):
+    return RateLimitItemPerSecond(tps)
+
+
+def ratelimit(tps):
+    def inner(func):
+        def wrapper(*args, **kwargs):
+            while not ratelimiter.hit(get_limit(tps)):
+                window = ratelimiter.get_window_stats(limit)
+                time.sleep(window.reset_time - int(datetime.now(timezone.utc).timestamp()))
+            return func(*args, **kwargs)
+        return wrapper
+    return inner
+
+
+@ratelimit(3)
+def do_work():
+    print('{} - {}'.format(datetime.now(), 'Do more work'))
+
+
+print('Using RateLimiter object directly')
 for _ in range(10):
     # The namespace/identifier is only needed if the same limit needs to be used for multiple
     # independent resources (e.g. use the same limit for different resources, but don't let the
@@ -17,3 +41,9 @@ for _ in range(10):
         print('{} - Waiting till {} ({} seconds)'.format(datetime.now(), reset_time, diff))
         time.sleep(diff)
     print('{} - {}'.format(datetime.now(), 'Do work'))
+
+
+print()
+print('Using wrapper')
+for _ in range(10):
+    do_work()
