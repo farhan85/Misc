7d829ce336e880a6b6a7e9cd750f3fe3e1e8da8c
farhan85 <4463436+farhan85@users.noreply.github.com>
2026-04-10 18:48:04 -0700

    Assume role script now loads an interactive shell

--- bash/assume-role.sh
+++ bash/assume-role.sh
@@ -3,21 +3,27 @@
 #ROLE_ARN="arn:aws:iam::$ACCOUNT:role/$ROLE_NAME"
 ROLE_ARN="$@"
 
-creds=$(aws sts assume-role --role-arn "$ROLE_ARN" \
-                            --role-session-name "session-$(date +%s)" \
-                            --query '[Credentials.AccessKeyId, Credentials.SecretAccessKey, Credentials.SessionToken]' \
-                            --output text)
+INIT_CMDS=$(cat <<EOF
+update-creds() {
+    NEW_CREDS=\$(aws sts assume-role \
+        --role-arn "$ROLE_ARN" \
+        --role-session-name "session-\$(date +%s)" \
+        --query '[Credentials.AccessKeyId, Credentials.SecretAccessKey, Credentials.SessionToken]' \
+        --output text)
+    if [ \$? -eq 0 ]; then
+        export AWS_ACCESS_KEY_ID=\$(echo \$NEW_CREDS | awk '{print \$1}')
+        export AWS_SECRET_ACCESS_KEY=\$(echo \$NEW_CREDS | awk '{print \$2}')
+        export AWS_SESSION_TOKEN=\$(echo \$NEW_CREDS | awk '{print \$3}')
+        echo "Updated credentials for $ROLE_ARN"
+    else
+        echo "Failed to load credentials."
+    fi
+}
 
-access_key_id=$(echo $creds | awk '{print $1}')
-secret_access_key=$(echo $creds | awk '{print $2}')
-session_token=$(echo $creds | awk '{print $3}')
+# Load creds on startup
+update-creds
+PS1='\[\e[33m\]assumed-role \[\e[m\]\w > '
+EOF
+)
 
-init_cmds="
-export AWS_DEFAULT_REGION=$AWS_DEFAULT_REGION
-export AWS_ACCESS_KEY_ID=$access_key_id
-export AWS_SECRET_ACCESS_KEY=$secret_access_key
-export AWS_SESSION_TOKEN=$session_token
-"
-
-# Start a bash shell with the creds pre loaded
-bash --init-file <(echo "$init_cmds")
+bash --init-file <(echo "$INIT_CMDS") -i
